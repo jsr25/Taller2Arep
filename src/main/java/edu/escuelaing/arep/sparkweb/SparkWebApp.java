5811abbd5a0507fb51bf25abad1dfc128c9b9f9a
jsr25 <ramosjuans@live.com.ar>
2021-01-31 09:39:44 -0500

    configuracion

--- src/main/java/edu/escuelaing/arep/sparkweb/SparkWebApp.java
+++ src/main/java/edu/escuelaing/arep/sparkweb/SparkWebApp.java
@@ -4,6 +4,17 @@ import static spark.Spark.*;
 public class SparkWebApp {
 
     public static void main(String[] args) {
-        get("/hello", (req, res) -> "Hello Heroku");
+        port(getPort());
+        get("/hello", (req, res) -> {
+            return res.status();
+        });
     }
+
+    static int getPort(){
+        if(System.getenv("PORT")!= null){
+            return  Integer.parseInt(System.getenv("PORT"));
+        }
+        return 4567;
+    }
+
 }
